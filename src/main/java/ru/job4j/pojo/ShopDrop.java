fe4957e9b11cb58c8d41ea7f055b38a7de85429b
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-11-22 17:38:11 +0300

    3.2. Удаление моделей из массива. [#310016 #230676]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -4,10 +4,7 @@ public class ShopDrop {
 
     public static Product[] delete(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
-            Product product = products[i];
-            if (product != null) {
                 products[i] = products[i + 1];
-            }
         }
         products[products.length - 1] = null;
         return products;
