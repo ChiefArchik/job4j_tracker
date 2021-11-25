d9c2ca60e38d55888abe1426e72fdbc6a88e29f2
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-11-25 10:08:24 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,13 +1,16 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
-    public void add(Item item) {
+    public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
+        return item;
     }
 
     public Item findById(int id) {
@@ -20,5 +23,34 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] allItems = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                allItems[size] = item;
+                size++;
+            }
+        }
+        allItems = Arrays.copyOf(allItems, size);
+        for (int i = 0; i < allItems.length; i++) {
+            System.out.println(allItems[i]);
+        }
+        return allItems;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] desiredItem = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                desiredItem[count++] = items[i];
+            }
+        }
+        desiredItem = Arrays.copyOf(desiredItem, size);
+        return desiredItem;
     }
 }
