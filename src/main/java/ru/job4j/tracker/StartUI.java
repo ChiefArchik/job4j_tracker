6e4bf3ffe48297d9e9c0f1b6288261937ca56827
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-11-26 15:15:20 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,5 +11,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String timeFormat = time.format(formatter);
         System.out.println(timeFormat);
+        System.out.println(item);
     }
 }
