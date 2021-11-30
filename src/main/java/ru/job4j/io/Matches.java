4c4132ce6ad4ec58aba51b1ee80bb4a44b1efe6b
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-11-30 11:55:24 +0300

    2. Scanner и чтение числа из консоли. [#179518 #234024]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,9 +12,13 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " Введите число от 1 до 3 : ");
             int matches = Integer.parseInt(input.nextLine());
-            turn = !turn;
-            count = count - matches;
-            System.out.println("Осталось спичек: " + count);
+            if (matches >= 1 && matches <= 3) {
+                turn = !turn;
+                count = count - matches;
+                System.out.println("Осталось спичек: " + count);
+            } else {
+                System.out.println("Число за пределами интервала.");
+            }
         }
         if (!turn) {
             System.out.println("Выиграл первый игрок.");
