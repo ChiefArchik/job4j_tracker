40bcd4c3384d0c837277f1c5d49709f7480be40e
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-12-01 16:06:43 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,31 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New Item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), "Edited Item"
+        };
+        StartUI.editItem(new SubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("Edited Item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUI.deleteItem(new SubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Item expected = null;
+        assertThat(deleted, is(expected));
+    }
 }
