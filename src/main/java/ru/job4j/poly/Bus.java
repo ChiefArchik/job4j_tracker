05e0b8438dab6e69ed01a63c7fdb7b1caf57587f
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-12-01 11:55:21 +0300

    3. Интерфейс Input [#181092 #234405]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Автобус едет.");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Колличество пассажиров: " + count);
     }
 
     @Override
     public int fuel(int liters) {
-        return 0;
+        int literPrice = 50;
+        return liters * literPrice;
     }
 }
