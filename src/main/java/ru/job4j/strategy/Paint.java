a03c29c7965965df17ff73357283b997132df77b
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-12-03 13:44:25 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/Paint.java
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import java.util.Scanner;
+
 public class Paint {
 
     public void draw(Shape shape) {
@@ -7,8 +9,18 @@ public class Paint {
     }
 
     public static void main(String[] args) {
+        System.out.println("Введите: 1, для вывода треугольника"
+        + " или: 2 для вывода квадрата.");
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
         Paint context = new Paint();
-        context.draw(new Triangle());
-        context.draw(new Square());
+        if (input == 1) {
+            context.draw(new Triangle());
+        } else if (input == 2) {
+            context.draw(new Square());
+        } else {
+            System.out.println("Ошибка ввода.");
+        }
+
     }
 }
