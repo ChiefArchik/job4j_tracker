1f7263e8cc4bbcf65eb83ad2ce256a4f4e583f3b
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2021-12-11 13:26:39 +0300

    0.4. Пользовательские исключения. [#219367 #238135]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,24 +1,10 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Throwable {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
-                rsl = i;
-            }
-        }
-        if (rsl == -1) {
-            throw new ElementNotFoundException();
-        }
-        System.out.println(rsl);
-        return rsl;
-    }
 
     public static void main(String[] args) {
         try {
-            indexOf(new String[]{"1", "3", "2"}, "4");
+            FindElement.indexOf(new String[]{"1", "3", "2"}, "4");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/ru/job4j/ex/FindElement.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class FindElement {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
+            }
+        }
+
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
+        }
+        System.out.println(rsl);
+        return rsl;
+    }
+}
