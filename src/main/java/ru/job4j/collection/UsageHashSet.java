a92d0050aff72efa02fab5a9aba69e87954a915c
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2022-01-19 18:49:01 +0300

    0. Set, HashSet, Iterator. [#10095 #252298]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -7,8 +7,15 @@ public class UsageHashSet {
         HashSet<String> autos = new HashSet<String>();
         autos.add("LADA");
         autos.add("Volvo");
-        autos.add("BMW");
         autos.add("Toyota");
+        autos.add("BMW");
+        autos.add("LADA");
+        autos.add("Toyota");
+        autos.add("Volvo");
+        autos.add("LADA");
+        autos.add("Toyota");
+        autos.add("Toyota");
+        autos.add("Volvo");
         for (String auto : autos) {
             System.out.println(auto);
         }
