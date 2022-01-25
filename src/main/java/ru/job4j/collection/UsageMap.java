d1957cc7cc1d5c1072b409e86b747e6c2690a415
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2022-01-25 13:09:29 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("chief.archik@yandex.ru", "Dmitry Artamonov");
+        map.put("chief.archik@rambler.ru", "Dmitry Artamonov");
+        map.put("chief.archik@mail.ru", "Dmitry Artamonov");
+        map.put("chief.archik@yandex.ru", "Dmitry Artamonov");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
