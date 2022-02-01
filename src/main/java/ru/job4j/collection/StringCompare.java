81c9d707416b69c06cedebfffc1387a0fdd57109
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2022-02-01 18:50:57 +0300

    3. Компаратор для строк [#35008]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,18 +5,13 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
         int less = Math.min(left.length(), right.length());
             for (int i = 0; i < less; i++) {
-                char leftChar = left.charAt(i);
-                char rightChar = right.charAt(i);
-                rsl = Character.compare(leftChar, rightChar);
+                int rsl = Character.compare(left.charAt(i), right.charAt(i));
                 if (rsl != 0) {
-                    break;
-                } else {
-                    rsl = Integer.compare(left.length(), right.length());
+                    return rsl;
                 }
             }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
