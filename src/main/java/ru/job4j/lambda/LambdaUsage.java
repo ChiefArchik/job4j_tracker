d301cc9b54bdd332ba8fe71bae10dc892c27fe7e
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2022-02-15 21:03:44 +0300

    2.2. Лямбда блок 2 [#249208]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -16,5 +16,16 @@ public class LambdaUsage {
         for (String str : strings) {
             System.out.println(str);
         }
+
+        String[] names = {
+                "Ivan",
+                "Dmitry"
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return Integer.compare(left.length(), right.length());
+        };
+        Arrays.sort(names, lengthCmp);
     }
+
 }
