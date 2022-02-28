e736ad4fe44333f9bd6527dbc6fad608a25b9039
ChiefArchik <git@github.com:ChiefArchik/job4j_elementary.git>
2022-02-28 18:16:14 +0300

    4. Преобразование List в Map. [#110226]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -2,14 +2,12 @@ package ru.job4j.stream;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ListToMap {
 
     public static Map<String, Student> convert(List<Student> list) {
         return list.stream()
-                .distinct()
                 .collect(Collectors.toMap(
                         Student::getSurname,
                         v -> v,
